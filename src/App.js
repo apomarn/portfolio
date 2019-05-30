5501dfada5f2576d8c5d21af6257fd474355baf0
Anghy <anghyf27@hotmail.com>
2019-05-30 18:23:13 -0400

    Skills Component for mobile

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from './sections/hero'
 import './App.css'
 import AboutMe from './sections/about-me'
+import Skills from './sections/skills'
 
 function App() {
   return (
     <div>
       <Hero />
       <AboutMe />
+      <Skills />
     </div>
   )
 }
